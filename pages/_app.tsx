29fefb3b4ca1d883bf4f6b08b8647e5fc2454752
malika_south.k <malikaatoeva96@gmail.com>
2024-09-15 10:54:34 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { light } from "../scsss/MaterialTheme";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import "../scsss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
