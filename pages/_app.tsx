7d9275e94941b6be90c6df901f8953a0ebfc5438
malika_south.k <malikaatoeva96@gmail.com>
2024-09-22 17:39:36 +0900

    feat: Apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { light } from "../scsss/MaterialTheme";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 import "../scsss/app.scss";
 import "../scsss/pc/main.scss";
 import "../scsss/mobile/main.scss";
@@ -13,9 +15,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   //Socket.io, Redux
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />;
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
